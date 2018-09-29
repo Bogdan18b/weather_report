22810b5511f84f0baa738db974d75bf83f2a57f2
bogdan <bogdanbobletec@gmail.com>
2018-09-29 10:25:16 -0400

    add city to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,13 @@
 import React from "react";
 
-const Header = () => (
-	<div>
-		<h1>Weather Report</h1>
-		<h3>Find out the temperature for the next 5 days</h3>
-	</div>
-);
+const Header = props => {
+	return (
+		<div>
+			{	props.city ? <h1>Weather Report for {props.city + ", " + props.country}</h1> : <h1>Weather Report</h1>
+			}
+			<h3>Find out the temperature for today and the next 5 days</h3>
+		</div>
+	);
+};
 
 export default Header;
