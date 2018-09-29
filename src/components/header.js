515884bc3b471167afef66bf83d12a92c259206f
bogdan <bogdanbobletec@gmail.com>
2018-09-28 22:41:55 -0400

    add header

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => (
+	<div>
+		<h1>Weather Report</h1>
+		<h3>Find out the temperature for the next 5 days</h3>
+	</div>
+);
+
+export default Header;
