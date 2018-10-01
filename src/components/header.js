207245b36b093099c629fd280f1748e48f13b867
bogdan <bogdanbobletec@gmail.com>
2018-10-01 11:23:03 -0400

    update subtitle

--- src/components/header.js
+++ src/components/header.js
@@ -8,7 +8,7 @@ const Header = props => {
 										 <h1 className="header-title">Weather Report</h1>
 			}
 			<h3 className="header-subtitle">
-				Find out the temperature for today and the next 5 days</h3>
+				Find out the temperature for the next 5 days</h3>
 		</div>
 	);
 };
