77a789f4477864e3663981df8106e78d8cec91fb
bogdan <bogdanbobletec@gmail.com>
2018-11-24 10:29:46 -0500

    Updates

---
+++ precache-manifest.b70ec0ab6b505a2429313106dcb2035e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "efb4a59f84148df98397",
+    "url": "/weather_report/static/js/runtime~main.efb4a59f.js"
+  },
+  {
+    "revision": "e4b12cb84a1415f601f5",
+    "url": "/weather_report/static/js/main.e4b12cb8.chunk.js"
+  },
+  {
+    "revision": "584dee9ce8761678bd16",
+    "url": "/weather_report/static/js/1.584dee9c.chunk.js"
+  },
+  {
+    "revision": "e4b12cb84a1415f601f5",
+    "url": "/weather_report/static/css/main.508510b1.chunk.css"
+  },
+  {
+    "revision": "63d30cb49529e82fbea84b4ad61e717b",
+    "url": "/weather_report/index.html"
+  }
+];
