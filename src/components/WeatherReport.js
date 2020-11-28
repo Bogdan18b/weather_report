f90b8856db69506eb09e18ef31108b1a1a514192
bogdan <bogdanbobletec@gmail.com>
2020-11-28 08:02:33 -0500

    fix useEffect bug

--- src/components/WeatherReport.js
+++ src/components/WeatherReport.js
@@ -1,28 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import Forecast from './Forecast';
 
-const GetWeather = async (city, country, degrees) => {
-  const result = await fetch(`//api.openweathermap.org/data/2.5/forecast?q=${city},${country}&appid=1ff011665e3637b91147bb172ff57517&units=${degrees}`);
-  const data = await result.json();
-  return data;
-}
-
-function useGetWeather(city, country, degrees) {
+const WeatherReport = ({ city, country, degrees }) => {
   const [data, setData] = useState({});
 
-  useEffect(
-    async () => {
-      const result = await GetWeather(city, country, degrees);
-      setData(result);
+  useEffect(() => {
+      const fetchData = async () => {
+        const response = await fetch(`//api.openweathermap.org/data/2.5/forecast?q=${city},${country}&appid=1ff011665e3637b91147bb172ff57517&units=${degrees}`);
+        const data = await response.json();
+        setData(data);
+      };
+      fetchData();
     },
-    [city, degrees]
+    [city, country, degrees]
   );
-  return data;
-}
-
-const WeatherReport = ({ city, country, degrees }) => {
-  const data = useGetWeather(city, country, degrees);
-
  return (
    <Forecast data={data} />
  );
